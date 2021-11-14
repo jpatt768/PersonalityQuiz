02c9d65f1ce9f1c961e9c6e767c843a7480db608
Jayden Patterson <jpatt768@gmail.com>
2021-11-14 13:49:34 +0900

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Jayden Patterson on 2021/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
